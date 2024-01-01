277b634447b22df0d1f30ca999b8de9bc7cec5ba
Sergey Karinsky <karinsky.dev@gmail.com>
2024-01-01 20:20:10 +0700

    Set button with via props

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,13 +1,17 @@
 import cn from 'classnames'
 import styles from './styles.module.scss'
 
-export default function Button({ children, className, isLoading, ...rest }) {
+export default function Button({ children, className, width, style, isLoading, ...rest }) {
   return (
     <button
       className={cn(styles.button, {
         [styles.button_loading]: isLoading,
         [className]: !!className
       })}
+      style={{
+        ...style,
+        width
+      }}
       {...rest}
     >
       {children}
