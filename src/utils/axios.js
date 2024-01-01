43f639273d87ab57b7207f652400feec577d0c68
Sergey Karinsky <karinsky.dev@gmail.com>
2024-01-01 20:17:10 +0700

    Add api key to request

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,10 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'https://www.googleapis.com/books/v1'
+  baseURL: 'https://www.googleapis.com/books/v1',
+  params: {
+    key: process.env.REACT_APP_API_KEY 
+  }
 })
 
 export default instance
